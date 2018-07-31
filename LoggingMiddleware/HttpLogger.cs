d22d1a23d8f98ccd205170d4ef3f4c7534ad36f7
Gerhard <geh@gmx.de>
2018-07-31 15:34:28 +0200

    Add project files.

---
+++ LoggingMiddleware/HttpLogger.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+using NLog;
+using System;
+using System.Security.Claims;
+
+namespace LoggingMiddleware
+{
+    public class HttpLogger
+    {
+        public static HttpLogger getLogger(string name) {
+            return new HttpLogger(name);
+        }
+
+        public const int REQUEST = 1;
+        public const int RESPONSE = 2;
+
+        private Logger log;
+
+        private HttpLogger(string name) {
+            log = LogManager.GetLogger(name);
+        }
+
+        public void Fatal(string msg, Exception ex = null)
+        {
+            log.Fatal(ex, msg);
+        }
+        public void Error(string msg, Exception ex = null)
+        {
+            log.Error(ex, msg);
+        }
+        public void Warn(string msg, Exception ex = null)
+        {
+            log.Warn(ex, msg);
+        }
+        public void Info(string msg, Exception ex = null)
+        {
+            log.Info(ex, msg);
+        }
+        public void Debug(string msg, Exception ex = null)
+        {
+            log.Debug(ex, msg);
+        }
+        public void Trace(string msg, Exception ex = null)
+        {
+            log.Trace(ex, msg);
+        }
+
+        private string formatLog(int type, HttpContext ctx, string msg1, string msg2) {
+            string msg;
+            switch (type) {
+                case REQUEST:
+                    ClaimsIdentity ci = (ClaimsIdentity)ctx.User.Identity;
+                    msg = ctx.Connection.RemoteIpAddress + "|"
+                              + ctx.Request.Host + "|" + ctx.Request.Method + "|"
+                              + ctx.TraceIdentifier + "|"
+                              + ctx.Request.Path + "|"
+                              + "Request|" + ci.FindFirst("Serial")?.Value + "|"
+                              + msg1 + "|" + msg2;
+                    break;
+                case RESPONSE:
+                    if (ctx.Response.StatusCode == 403)
+                    {
+                        JObject rc = new JObject();
+                        ClaimsIdentity ci2 = (ClaimsIdentity)ctx.User.Identity;
+                        foreach ( Claim c in ci2.Claims )
+                        {
+                            rc.Add(c.Type, c.Value);
+                        }
+                        msg1 = rc.ToString();
+                    }
+                    msg = ctx.Connection.RemoteIpAddress + "|"
+                              + ctx.Request.Host + "|" + ctx.Request.Method + "|"
+                              + ctx.TraceIdentifier + "|"
+                              + ctx.Request.Path + "|"
+                              + "Response|" + ctx.Response.StatusCode + "|"
+                              + msg1 + "|" + msg2;
+                    break;
+                default:
+                    msg = ctx.Connection.RemoteIpAddress + "|Unknown";
+                    break;
+            }
+            return msg;
+        }
+
+        public void Fatal(int type, HttpContext ctx, string msg1 = null, string msg2 = null)
+        {
+            log.Fatal(formatLog(type, ctx, msg1, msg2));
+        }
+        public void Error(int type, HttpContext ctx, string msg1 = null, string msg2 = null)
+        {
+            log.Error(formatLog(type, ctx, msg1, msg2));
+        }
+        public void Warn(int type, HttpContext ctx, string msg1 = null, string msg2 = null)
+        {
+            log.Warn(formatLog(type, ctx, msg1, msg2));
+        }
+        public void Info(int type, HttpContext ctx, string msg1 = null, string msg2 = null)
+        {
+            log.Info(formatLog(type, ctx, msg1, msg2));
+        }
+        public void Debug(int type, HttpContext ctx, string msg1 = null, string msg2 = null)
+        {
+            log.Debug(formatLog(type, ctx, msg1, msg2));
+        }
+        public void Trace(int type, HttpContext ctx, string msg1 = null, string msg2 = null, string msg3 = null)
+        {
+            log.Trace(formatLog(type, ctx, msg1, msg2));
+        }
+
+    }
+}
---
+++ LoggingMiddleware/LoggingMiddleware.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using NLog;
+using NLog.Common;
+using NLog.Web;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LoggingMiddleware
+{
+    public static class LoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLoggingMiddleware(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoggingMiddleware>();
+        }
+    }
+
+    public class LoggingMiddleware
+    {
+        private static LogFactory logFactory;
+        private static HttpLogger log = HttpLogger.getLogger(nameof(LoggingMiddleware));
+        private readonly RequestDelegate _next;
+
+        private static int logOption = LOG_DEFAULT;
+        public const int LOG_DEFAULT = 0;
+        public const int LOG_FULL_RESPONSE = 0b00000001;
+
+        public static void init()
+        {
+            // Define variables and initialize NLog
+            string domainName = AppDomain.CurrentDomain.FriendlyName;
+            string logdir = AppContext.BaseDirectory + "/../data";
+            GlobalDiagnosticsContext.Set("domainName", domainName);
+            GlobalDiagnosticsContext.Set("logdir", logdir);
+            InternalLogger.LogFile = logdir + "/" + domainName + "_internal.log";
+
+            logFactory = NLogBuilder.ConfigureNLog("nlog.config");
+        }
+
+        public static void setLogOption(int aLogOption)
+        {
+            logOption = aLogOption;
+        }
+
+        public static Logger getLoger()
+        {
+            return logFactory.GetCurrentClassLogger();
+        } 
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        //see https://exceptionnotfound.net/using-middleware-to-log-requests-and-responses-in-asp-net-core/?utm_campaign=Revue%20newsletter&utm_medium=Newsletter&utm_source=ASP.NET%20Weekly
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            log.Info(HttpLogger.REQUEST, httpContext);
+
+            var originalBodyStream = httpContext.Response.Body;
+
+            using (var responseBody = new MemoryStream())
+            {
+                httpContext.Response.Body = responseBody;
+
+                try {
+                    await this._next(httpContext);
+                } catch (Exception ex)
+                {
+                    log.Error(HttpLogger.REQUEST, httpContext);
+                    log.Error(ex.Message);
+                    throw;
+                }
+
+                bool bFullResponse = Convert.ToBoolean( logOption & LOG_FULL_RESPONSE );
+
+                string response = await FormatResponse(httpContext.Response);
+                log.Info(HttpLogger.RESPONSE,
+                    httpContext,
+                    response.Length.ToString(), bFullResponse ? response : "");
+
+                await responseBody.CopyToAsync(originalBodyStream);
+            }
+        }
+
+        private async Task<string> FormatResponse(HttpResponse response)
+        {
+            //We need to read the response stream from the beginning...
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            //...and copy it into a string
+            string text = await new StreamReader(response.Body).ReadToEndAsync();
+
+            //We need to reset the reader for the response so that the client can read it.
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            //Return the string for the response, including the status code (e.g. 200, 404, 401, etc.)
+            return $"{text}";
+        }
+
+    }
+}
